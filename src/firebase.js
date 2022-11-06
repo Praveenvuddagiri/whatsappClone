a62499d24263e07eec803b49fa18b4d819faa772
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2022-11-06 15:33:53 +0530

    fixed small bugs

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,14 @@ import firebase from 'firebase/compat/app'
 import "firebase/compat/firestore"
 import "firebase/compat/auth";
 
-const firebaseApp = firebase.initializeApp({
-
-  apiKey: "AIzaSyCz-pgWbR-uJlR6Q9upeSin3u2qAWy0bnA",
-  authDomain: "contact-list-app-93bab.firebaseapp.com",
-  projectId: "contact-list-app-93bab",
-  storageBucket: "contact-list-app-93bab.appspot.com",
-  messagingSenderId: "156509237248",
-  appId: "1:156509237248:web:f0e9686a975d6d7e7e28e5",
-  measurementId: "G-R0CCSRVYLW"
+firebase.initializeApp({
+  apiKey: "AIzaSyDPpYdjilRTx93E3utvWhGtgELQSyZkpNw",
+  authDomain: "whatsappmern-f91ff.firebaseapp.com",
+  projectId: "whatsappmern-f91ff",
+  storageBucket: "whatsappmern-f91ff.appspot.com",
+  messagingSenderId: "75054761289",
+  appId: "1:75054761289:web:f54a6e4880e60d339ac698",
+  measurementId: "G-33ZRJL7Z62"
 });
 
 export const auth = firebase.auth();
